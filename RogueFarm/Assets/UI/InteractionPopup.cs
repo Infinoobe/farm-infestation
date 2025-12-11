9830e57128b9eb57c61c3d6cf0b048913595bc3d
NamespaceV <s-zephyr@o2.pl>
2025-12-12 00:00:05 +0100

    place interaction popup over item

--- RogueFarm/Assets/UI/InteractionPopup.cs
+++ RogueFarm/Assets/UI/InteractionPopup.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class InteractionPopup : MonoBehaviour
@@ -19,6 +20,14 @@ public class InteractionPopup : MonoBehaviour
         interactionBG.gameObject.SetActive(hasInteraction);
         if (hasInteraction)
         {
+            var mainCamera = Camera.main;
+            Vector2 screenPos = mainCamera.WorldToScreenPoint(i.GetPosition());
+            var parentRect = interactionBG.rectTransform.parent as RectTransform;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+               parentRect, screenPos, null, out Vector2 localPoint);
+            localPoint.y += interactionBG.rectTransform.rect.height / 2;
+            interactionBG.rectTransform.anchoredPosition = localPoint;
+            
             interactionLabel.text = $"Press E to {i.GetDescription()}";
         }
     }
