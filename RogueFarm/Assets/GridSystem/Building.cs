0c7e6f40fe23094863c955e7e15f27fcf652eb23
Gab-Master <Gabryel.jun@gmail.com>
2026-03-27 18:04:01 +0100

    created layers

--- RogueFarm/Assets/GridSystem/Building.cs
+++ RogueFarm/Assets/GridSystem/Building.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class Building : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private bool canBeTargeted;
+    [SerializeField] private bool canBeDestroyed;
+    [SerializeField] private bool canBeWalkedOn;
+    [SerializeField] private int health;
+    private bool isPlaced;
+    private GridCell[] occupiedCells;
+
+    public bool CanBeTargeted => canBeTargeted;
+    public bool CanBeWalkedOn => canBeWalkedOn;
+    public int CurrHealth => health;
+
+    public void PlaceBuilding(GridCell[] onCells)
     {
-        
+        occupiedCells = onCells;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RemoveBuilding()
     {
-        
+        if (!canBeDestroyed) return;
+        foreach(GridCell cell in occupiedCells){
+            cell.UnSetBuilding();
+        }
+        // TODO: returning some resources
+        DestroyBuilding();
     }
+
+    public void DestroyBuilding()
+    {
+        Destroy(gameObject);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health = Mathf.Max(0, health-damage);
+        if (health == 0) DestroyBuilding();
+    }
+    
 }
