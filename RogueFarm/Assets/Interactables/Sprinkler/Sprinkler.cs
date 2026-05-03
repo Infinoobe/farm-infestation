4a36fd4a190b6d0486d1cd7c4b84946678eab38f
Gab-Master <Gabryel.jun@gmail.com>
2026-05-03 13:09:22 +0200

    sprinkler
    pls dont kill me. hehe

---
+++ RogueFarm/Assets/Interactables/Sprinkler/Sprinkler.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using Interactable.Common;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Sprinkler : Building
+{
+    [SerializeField] private ParticleSystem waterEffect;
+
+    public override void Interact(Player p)
+    {
+        base.Interact(p);
+
+        waterEffect.Play();
+        SprinkleFields();
+    }
+
+    public override ActionType GetDescription(out string message)
+    {
+        message = "Click to sprinkle fields";
+        return ActionType.INTERACTION;
+    }
+
+    private void SprinkleFields()
+    {
+        GridCell mainCell = occupiedCells[0]; // assume sprinkler size = 1
+        List<GridCell> cells = mainCell.myGrid.grid.GetGridCellsInRange(mainCell, influenceRange);
+        foreach (GridCell cell in cells)
+        {
+            if(cell.currBuilding.TryGetComponent<Field>(out var field))
+            {
+                field.WaterField();
+            }
+
+        }
+    }
+    
+}
